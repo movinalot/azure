52315770db372d2e5fcfa2778951b149fc67fa89
John McDonough <movinalot@gmail.com>
2022-03-15 15:27:13 -0400

    add virtual_network_peering

---
+++ rm/azurerm_virtual_network_peering/azurerm_virtual_network_peering.tf
@@ -0,0 +1,15 @@
+resource "azurerm_virtual_network_peering" "virtual_network_peering" {
+
+  resource_group_name = var.resource_group_name
+
+  name                         = var.name
+  virtual_network_name         = var.virtual_network_name
+  remote_virtual_network_id    = var.remote_virtual_network_id
+  allow_virtual_network_access = var.allow_virtual_network_access
+  allow_forwarded_traffic      = var.allow_forwarded_traffic
+  use_remote_gateways          = var.use_remote_gateways
+}
+
+output "virtual_network_peering" {
+  value = azurerm_virtual_network_peering.virtual_network_peering
+}
---
+++ rm/azurerm_virtual_network_peering/variables.tf
@@ -0,0 +1,27 @@
+variable "resource_group_name" {
+  description = "resource_group_name"
+}
+
+variable "name" {
+  description = "name"
+}
+
+variable "virtual_network_name" {
+  description = "virtual_network_name"
+}
+
+variable "remote_virtual_network_id" {
+  description = "remote_virtual_network_id"
+}
+
+variable "allow_virtual_network_access" {
+  description = "allow_virtual_network_access"
+}
+
+variable "allow_forwarded_traffic" {
+  description = "allow_forwarded_traffic"
+}
+
+variable "use_remote_gateways" {
+  description = "use_remote_gateways"
+}
