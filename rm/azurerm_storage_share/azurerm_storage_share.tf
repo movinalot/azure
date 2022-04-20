f70de55c93ca53ebb9d04e6102e13bf21a4345ee
John McDonough <movinalot@gmail.com>
2022-04-19 23:48:05 -0400

    update to azurerm 3.X

--- rm/azurerm_storage_share/azurerm_storage_share.tf
+++ rm/azurerm_storage_share/azurerm_storage_share.tf
@@ -2,6 +2,7 @@ resource "azurerm_storage_share" "storage_share" {
 
   name                 = var.name
   storage_account_name = var.storage_account_name
+  quota                = var.quota
 }
 
 output "storage_share" {
--- rm/azurerm_storage_share/variables.tf
+++ rm/azurerm_storage_share/variables.tf
@@ -5,3 +5,7 @@ variable "name" {
 variable "storage_account_name" {
   description = "storage_account_name"
 }
+
+variable "quota" {
+  description = "quota"
+}
